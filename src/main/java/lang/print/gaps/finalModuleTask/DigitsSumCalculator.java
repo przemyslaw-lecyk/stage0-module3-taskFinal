b29c89b1176375af2dc585a016e34834c6bff1a7
Stanislau_Sukora <ssukora@estee.com>
2023-07-21 10:43:49 +0300

    Misc updates

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+        int sum = 0;
+        while (number != 0){
+            int reminder = number % 10;
+            sum = sum + reminder;
+            number = number / 10;
+        }
+        System.out.println(sum);
     }
 }
--- src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
+++ src/main/java/lang/print/gaps/finalModuleTask/NumberReverter.java
@@ -2,5 +2,12 @@ package lang.print.gaps.finalModuleTask;
 
 public class NumberReverter {
     public void revert(int number) {
+        int revertedNumber = 0;
+        while (number != 0){
+            int reminder = number % 10;
+            revertedNumber = revertedNumber * 10 + reminder;
+            number = number / 10;
+        }
+        System.out.println(revertedNumber);
     }
 }
